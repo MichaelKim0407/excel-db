8ce96e2de5210a2c2b3bf80c945afe15be0dd06f
Zheng Jin <mkim0407@gmail.com>
2023-06-01 11:47:09 -0700

    implement ColumnsStartWith

--- src/excel_models/columns/multi.py
+++ src/excel_models/columns/multi.py
@@ -35,3 +35,45 @@ class Columns(BaseContainer):
     def from_python(self, value: typing.Sequence) -> typing.Sequence:
         for v in value:
             yield self.inner.from_python(v)
+
+
+class ColumnsStartWith(BaseContainer):
+    create_keys: typing.Collection[str]
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.column_class is None:
+            from excel_models.column_inst.map import ExcelColumnMap
+            self.column_class = ExcelColumnMap
+
+    def match_column(self, table: TTable, col_num: int) -> TColumn | None:
+        title = table.get_title(col_num)
+        if not title.startswith(self.name):
+            return None
+
+        key = title[len(self.name):]
+
+        if self.attr in table.columns_cache:
+            column = table.columns_cache[self.attr]
+            column.col_map[key] = col_num
+        else:
+            return self.column_class(table, self, {key: col_num})
+
+    def init_column(self, table: TTable, col_num: int) -> tuple[TColumn, int]:
+        col_map = {
+            k: c
+            for c, k in enumerate(self.create_keys, start=col_num)
+        }
+        for k, c in col_map.items():
+            table.set_title(c, f'{self.name}{k}')
+        return self.column_class(table, self, col_map), len(col_map)
+
+    @returns(dict)
+    def to_python(self, raw: typing.Mapping) -> typing.Mapping:
+        for k, v in raw.items():
+            yield k, self.inner.to_python(v)
+
+    @returns(dict)
+    def from_python(self, value: typing.Mapping) -> typing.Mapping:
+        for k, v in value.items():
+            yield k, self.inner.from_python(v)
---
+++ src/tests/test_columns/test_columns_start_with.py
@@ -0,0 +1,63 @@
+import pytest
+
+from excel_models.column_inst.map import ExcelColumnMap
+from excel_models.columns.basic_types import IntColumn
+from excel_models.columns.multi import ColumnsStartWith
+from excel_models.db import ExcelDB
+from excel_models.models import ExcelModel
+
+
+@pytest.fixture()
+def excel(lazy_init_excel):
+    return lazy_init_excel('numbers', ['number', 'number2', 'number3'], [1, 2, 3], [4, 5, '6'], ['7', 8, 9])
+
+
+class Numbers(ExcelModel):
+    numbers = ColumnsStartWith(name='number', inner=IntColumn(), create_keys=('1', '2', '3'))
+
+    @property
+    def sum(self) -> int:
+        return sum(self.numbers.values())
+
+
+class MyDB(ExcelDB):
+    numbers = Numbers.as_table()
+    numbers2 = Numbers.as_table()
+
+
+@pytest.fixture()
+def db(excel):
+    return MyDB(excel)
+
+
+def test_read(db):
+    column = db.numbers.numbers
+    assert isinstance(column, ExcelColumnMap)
+    assert column.col_map == {'': 1, '2': 2, '3': 3}
+
+    n0 = db.numbers[0]
+    assert n0.numbers == {'': 1, '2': 2, '3': 3}
+    assert n0.sum == 6
+
+    n1 = db.numbers[1]
+    assert n1.numbers == {'': 4, '2': 5, '3': 6}
+    assert n1.sum == 15
+
+    n2 = db.numbers[2]
+    assert n2.numbers == {'': 7, '2': 8, '3': 9}
+    assert n2.sum == 24
+
+
+def test_write(db):
+    column = db.numbers2.numbers
+    assert isinstance(column, ExcelColumnMap)
+    assert column.col_map == {'1': 1, '2': 2, '3': 3}
+    assert db.numbers2.cell(1, 1).value == 'number1'
+    assert db.numbers2.cell(1, 2).value == 'number2'
+    assert db.numbers2.cell(1, 3).value == 'number3'
+
+    new = db.numbers2.new()
+    new.numbers = {'1': 1, '2': 2, '3': 3}
+    assert db.numbers2.cell(2, 1).value == 1
+    assert db.numbers2.cell(2, 2).value == 2
+    assert db.numbers2.cell(2, 3).value == 3
